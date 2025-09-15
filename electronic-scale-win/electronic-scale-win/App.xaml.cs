83f9fdd72d49c7f887cdf26193e09d508775e71c
麦草 <vaemc520@qq.com>
2025-09-15 16:53:48 +0800

    1

---
+++ electronic-scale-win/electronic-scale-win/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace electronic_scale_win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
